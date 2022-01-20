e9800a52ef1561421a6f164ffd85f43b0fff28a9
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-20 14:40:21 +0700

    added MVC to app

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,5 +3,5 @@
     <h1>About Page</h1>
     <h3>{{  $name }}</h3>
     <p>{{ $email }}</p>
-    <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,53 +31,19 @@ Route::get('/about', function () {
 
 
 Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "First title post",
-            "slug" => "first-title-post",
-            "author" => "Frans Sebastian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, quia culpa suscipit expedita quaerat magnam neque deserunt corrupti asperiores voluptates animi sequi placeat laborum unde nemo hic excepturi deleniti ipsum id quasi numquam vitae ullam. Rem itaque non nulla commodi ratione corrupti tempora incidunt maxime facilis nam necessitatibus nesciunt est esse sit delectus sint adipisci, quo sed optio aliquid hic sunt, voluptates sequi voluptas. Nesciunt porro quia, iste, vero architecto fugiat soluta illum cum a, ex aperiam et maiores distinctio."
-        ],
-        [
-            "title" => "Second title post",
-            "slug" => "second-title-post",
-            "author" => "Frans Sebastian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque magnam deleniti tenetur quasi eligendi, aspernatur vitae explicabo beatae ratione, debitis dolor exercitationem velit tempore illo eveniet aperiam maiores nihil est. Delectus explicabo incidunt suscipit nulla, laboriosam sequi perferendis, similique modi aspernatur aperiam eaque! Aliquid, voluptatum sunt dolorem repellendus quo voluptates tempore, voluptatem necessitatibus ratione iste omnis porro fugiat neque laboriosam nulla explicabo. Facere nemo porro voluptatum placeat nisi, cum repellendus odit recusandae adipisci quo neque vel sit autem pariatur molestiae consequatur ipsa deserunt laudantium iusto consequuntur iste ipsum voluptates aut nam. Nobis at doloribus voluptas voluptatibus non distinctio consequuntur sapiente?"
-        ]
-
-    ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts,
-    ]);
 });
 
 /* Halaman single post */
 Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "First title post",
-            "slug" => "first-title-post",
-            "author" => "Frans Sebastian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, quia culpa suscipit expedita quaerat magnam neque deserunt corrupti asperiores voluptates animi sequi placeat laborum unde nemo hic excepturi deleniti ipsum id quasi numquam vitae ullam. Rem itaque non nulla commodi ratione corrupti tempora incidunt maxime facilis nam necessitatibus nesciunt est esse sit delectus sint adipisci, quo sed optio aliquid hic sunt, voluptates sequi voluptas. Nesciunt porro quia, iste, vero architecto fugiat soluta illum cum a, ex aperiam et maiores distinctio."
-        ],
-        [
-            "title" => "Second title post",
-            "slug" => "second-title-post",
-            "author" => "Frans Sebastian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque magnam deleniti tenetur quasi eligendi, aspernatur vitae explicabo beatae ratione, debitis dolor exercitationem velit tempore illo eveniet aperiam maiores nihil est. Delectus explicabo incidunt suscipit nulla, laboriosam sequi perferendis, similique modi aspernatur aperiam eaque! Aliquid, voluptatum sunt dolorem repellendus quo voluptates tempore, voluptatem necessitatibus ratione iste omnis porro fugiat neque laboriosam nulla explicabo. Facere nemo porro voluptatum placeat nisi, cum repellendus odit recusandae adipisci quo neque vel sit autem pariatur molestiae consequatur ipsa deserunt laudantium iusto consequuntur iste ipsum voluptates aut nam. Nobis at doloribus voluptas voluptatibus non distinctio consequuntur sapiente?"
-        ]
-    ];
-
-    $new_post = [];
+    /*   $new_post = [];
     foreach ($blog_posts as $post) {
         if ($post["slug"] === $slug) {
             $new_post = $post;
         }
-    }
+    } */
 
     return view('post', [
         "title" => "Single Post",
-        "post" => $new_post
+        "post" => Post::find($slug)
     ]);
 });
