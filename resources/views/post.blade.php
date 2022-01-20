03bad272565c9c765ec87d4f90c668c8eff3e072
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-20 13:06:55 +0700

    blade templating engine

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.main')
-
-@section('container')
-    <article>
-        <h2>Judul</h2>
-        <h4>Author</h4>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, deserunt delectus illum, culpa sint ab modi consequatur perspiciatis veniam aspernatur nesciunt molestias ex eius impedit, non laboriosam fuga vel dolore.</p>
-    </article>
-
-@endsection
+    @section('container')
+        <article>
+            <h2>{{ $post["title"] }}</h2>
+            <h4>{{ $post["author"] }}</h4>
+            <p>L{{ $post["body"] }}</p>
+        </article>
+        <a href="/blog">Back to post</a>
+    @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,7 @@
     @foreach ($posts as $post)
         <article class="mb-5">
             <h2>
-                <a href="{{ $post['slug'] }}">{{ $post["title"] }}</a>
+                <a href="/posts/{{ $post['slug'] }}">{{ $post["title"] }}</a>
             </h2>
             <h5>By: {{ $post["author"] }}</h5>
             <p>{{ $post["body"] }}</p>
