8824eeae6f5e6bb45664dd71ca2d8e86586af527
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-05 16:06:34 +0700

    made category controller

--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,8 @@ Route::get('/dashboard', function () {
 Route::get('dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
+// Route untuk categories
+Route::resource('/dashboard/categories', AdminCategoryController::class);
 
 
 
